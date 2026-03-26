0badc48e364d61461bff19ade75e92783ff2c54f
Aden Lazarus <adenlazarus@gmail.com>
2026-03-26 22:35:06 +0100

    build: add AppConfig, ControlServer sources and link httplib

---
+++ src/server/ControlServer.cpp
@@ -0,0 +1,7 @@
+// ControlServer.cpp
+// HTTP server with REST API and Server-Sent Events (SSE)
+// Stub for Task 4 - to be implemented in Task 14
+
+#include "ControlServer.h"
+
+// Placeholder implementation - will be fully implemented in Task 14
